f5eea030cc2bfb8121ccbc19cb41be38f258f50e
YassineCHN <yassine.0806@live.fr>
2025-11-11 20:10:29 +0100

    R6 structure

---
+++ R/models.R
@@ -0,0 +1,22 @@
+clust <- R6Classe( 
+  "Clustering", 
+  private=list(
+    
+  ),
+  publics=list(
+    fit=function() {
+      
+    }
+    
+    predict=function() {
+      
+    }
+    print=function() {
+      
+    }
+    summary=function() {
+      
+    }
+  )
+  
+  )
